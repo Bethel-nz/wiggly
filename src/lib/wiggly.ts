52a326c2da26f3141913e5128c9d3418b2a96917
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-18 23:32:08 +0100

    feat: added support for deeply nested routes fix: refactor and broke down each modules into reusable functions feat: added doctring for easy documentation

--- src/lib/wiggly.ts
+++ src/lib/wiggly.ts
@@ -2,22 +2,35 @@ import { serve as node_serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import fs from 'fs';
 import path from 'path';
-import generate_types from '../utils/generate-types';
 import chokidar from 'chokidar';
+
+/**
+ * The `Wiggly` class is a file-based routing system for the Hono.js framework.
+ * It dynamically loads route handlers and middleware from specified directories and applies them to the Hono application instance.
+ * @class Wiggly
+ */
 class Wiggly {
   private app: Hono;
   private default_dir: string;
   private default_middleware_dir: string;
 
+  /**
+   * Creates an instance of the `Wiggly` class.
+   * @param default_args - Configuration object for initializing the `Wiggly` instance.
+   * @param default_args.app - Optional Hono application instance to use. If not provided, a new instance will be created.
+   * @param default_args.base_path - Optional base path to set for the Hono application.
+   * @param default_args.middleware_dir - Optional directory path for middleware files.
+   * @param default_args.routes_dir - Optional directory path for route files.
+   */
   constructor(default_args: {
     app?: Hono;
     base_path?: string;
     middleware_dir?: string;
     routes_dir?: string;
   }) {
-    this.app = default_args['app']
-      ? default_args['app']
-      : new Hono().basePath(default_args['base_path']!);
+    this.app = default_args.app
+      ? default_args.app
+      : new Hono().basePath(default_args.base_path!);
 
     const currentDir = process.cwd();
     this.default_middleware_dir = default_args.middleware_dir
@@ -29,142 +42,235 @@ class Wiggly {
     this.applyGlobalMiddleware();
   }
 
+  /**
+   * Checks if a given file path has a valid extension (.js or .ts).
+   * @param file_path - The path of the file to check.
+   * @returns `true` if the file has a valid extension, `false` otherwise.
+   */
   private is_valid_file(file_path: string): boolean {
     return ['.js', '.ts'].includes(path.extname(file_path));
   }
 
+  /**
+   * Parses a route segment from a file or directory name.
+   * Converts segments enclosed in square brackets to a parameterized format (e.g., `[id]` to `:id`).
+   * @param segment - The route segment to parse.
+   * @returns The parsed route segment.
+   */
   private parse_route_segment(segment: string): string {
     if (segment.startsWith('[') && segment.endsWith(']')) {
-      if (segment.startsWith('[...') && segment.endsWith(']')) {
-        return `:${segment.slice(4, -1)}*`;
-      } else {
-        return `:${segment.slice(1, -1)}`;
-      }
+      return `:${segment.slice(1, -1)}`;
     }
     return segment;
   }
+
+  /**
+   * Checks if a given file name is a middleware file based on its name.
+   * @param file_name - The name of the file to check.
+   * @returns `true` if the file is a middleware file, `false` otherwise.
+   */
   private is_middleware_file(file_name: string): boolean {
     return (
       file_name.startsWith('_middleware') || file_name.startsWith('_index')
     );
   }
 
-  private applyMiddleware(directory: string, base_path: string = '/'): void {
-    const middlewarePath = path.join(directory, '_middleware.ts');
-    const indexMiddlewarePath = path.join(directory, '_index.ts');
+  /**
+   * Applies middleware to the specified directory and path.
+   * @param directory - The directory containing middleware files.
+   * @param basePath - The base path for the middleware application.
+   */
+  private applyMiddleware(directory: string, basePath: string = '/'): void {
+    const middlewareFiles = [
+      path.join(directory, '_middleware.ts'),
+      path.join(directory, '_index.ts'),
+    ];
 
-    [middlewarePath, indexMiddlewarePath].forEach((middlewareFilePath) => {
+    middlewareFiles.forEach((filePath) => {
       if (
-        fs.existsSync(middlewareFilePath) &&
-        this.is_valid_file(middlewareFilePath) &&
-        fs.statSync(middlewareFilePath).size > 0 // Check if file is not empty
+        fs.existsSync(filePath) &&
+        this.is_valid_file(filePath) &&
+        fs.statSync(filePath).size > 0
       ) {
-        const middleware = require(middlewareFilePath).default._;
+        const middleware = require(filePath).default._;
         if (typeof middleware === 'function') {
-          this.app.use(base_path, middleware);
+          this.app.use(
+            `/${path.relative(this.default_dir, directory)}/*`,
+            middleware
+          );
         }
-      } else {
-        return;
       }
     });
   }
-  private applyGlobalMiddleware(): void {
-    if (
-      this.default_middleware_dir &&
-      fs.existsSync(this.default_middleware_dir)
-    ) {
-      const files = fs.readdirSync(this.default_middleware_dir);
 
-      files.forEach((file) => {
-        const file_path = path.join(this.default_middleware_dir, file);
+  /**
+   * Applies global middleware functions from files in the default middleware directory.
+   * Middleware files must be named `_middleware.ts` or `_index.ts`.
+   */
+
+  private applyGlobalMiddleware(): void {
+    if (fs.existsSync(this.default_middleware_dir)) {
+      fs.readdirSync(this.default_middleware_dir).forEach((file) => {
+        const filePath = path.join(this.default_middleware_dir, file);
+
         if (
-          this.is_valid_file(file_path) &&
+          this.is_valid_file(filePath) &&
           this.is_middleware_file(file) &&
-          fs.statSync(file_path).size > 0 // Check if file is not empty
+          fs.statSync(filePath).size > 0
         ) {
-          const middleware = require(file_path).default._;
+          const middleware = require(filePath).default._;
           if (typeof middleware === 'function') {
             this.app.use('*', middleware);
           }
         }
       });
-    } else {
-      return;
     }
   }
 
-  private convertToHonoRoute(file_path: string, base_path: string): string {
-    const route_name = path.basename(file_path, path.extname(file_path));
-    if (route_name.startsWith('_')) return '';
-    const isIndexFile = route_name.startsWith('index');
+  /**
+   * Converts a file path to a Hono route path.
+   * @param filePath - The path of the file to convert.
+   * @returns The Hono route path corresponding to the file.
+   */
+  private convertToHonoRoute(filePath: string): string {
+    const routeName = path.basename(filePath, path.extname(filePath));
 
-    const relativePath = path.relative(this.default_dir, file_path);
+    // Ignore files starting with '_'
+    if (routeName.startsWith('_')) return '';
+
+    // Determine if the file is an index file
+    const isIndexFile = routeName === 'index';
+
+    // Create the route path from the directory structure and file name
+    const relativePath = path.relative(this.default_dir, filePath);
     const dirPath = path.dirname(relativePath);
-    const pathSegments = dirPath.split(path.sep).map(this.parse_route_segment);
+    const pathSegments = dirPath
+      .split(path.sep)
+      .map(this.parse_route_segment)
+      .filter(Boolean); // Filter out empty segments
 
-    const finalRouteName = isIndexFile ? '' : `/${route_name}`;
-    const routePath = pathSegments.join('/') + finalRouteName;
+    // Construct the final route path
+    const finalRouteName = isIndexFile ? '' : `/${routeName}`;
+    const routePath = `/${[...pathSegments, finalRouteName].join('/')}`
+      .replace(/\/+/g, '/') // Remove duplicate slashes
+      .replace(/\/$/, ''); // Remove trailing slash
 
-    return routePath.replace(/\[(\.\.\.)?(\w+)\]/g, (_, spread, param) => {
-      return spread ? `:${param}*` : `:${param}`;
-    });
+    return routePath.replace(/\[(\w+)\]/g, ':$1');
   }
+
+  /**
+   * Builds routes based on files in the specified directory.
+   * Handles both directories and individual files, applying middleware and route handlers.
+   * @param directory - The directory to build routes from.
+   * @param base_path - Optional base path to prepend to all routes.
+   */
   build_routes(
     directory: string = this.default_dir,
     base_path: string = ''
   ): void {
     if (!fs.existsSync(directory)) {
       console.log(
-        `Directory "${directory}" does not exist. Please specify a valid routes directory in the Wiggly configuration. or make a "/routes" or "/src/routes" in your root folder`
+        `Directory "${directory}" does not exist. Please specify a valid routes directory in the Wiggly configuration or create a "/routes" or "/src/routes" folder in your root directory.`
       );
       return;
     }
+
     const files = fs.readdirSync(directory);
+
     files.forEach((file) => {
       const file_path = path.join(directory, file);
       const stat = fs.statSync(file_path);
 
       if (stat.isDirectory()) {
-        const segment = this.parse_route_segment(file);
-        this.build_routes(file_path, `/${segment}`);
+        this.handleDirectory(file_path, base_path);
       } else if (this.is_valid_file(file_path) && stat.size > 0) {
-        const route = require(file_path).default;
-        const base_name = `/${path.basename(path.dirname(file_path))}`;
-
-        const route_path = this.convertToHonoRoute(file_path, base_name);
-        this.applyMiddleware(directory, base_name);
-
-        Object.keys(route).forEach((method) => {
-          const handler = route[method];
-          if (typeof handler === 'function') {
-            switch (method.toLowerCase()) {
-              case 'get':
-                this.app.get(route_path!, handler);
-                break;
-              case 'post':
-                this.app.post(route_path!, handler);
-                break;
-              case 'put':
-                this.app.put(route_path!, handler);
-                break;
-              case 'delete':
-                this.app.delete(route_path!, handler);
-                break;
-              case 'patch':
-                this.app.patch(route_path!, handler);
-                break;
-              case '_':
-                this.app.use(route_path!, handler);
-                break;
-              default:
-                console.warn(`Unknown method ${method} in file ${file_path}`);
-            }
-          }
-        });
-      } else return;
+        this.handleFile(file_path, directory);
+      }
     });
   }
 
+  /**
+   * Handles route directories, including nested directories and index files.
+   * @param dirPath - The path of the directory to handle.
+   * @param basePath - The base path to prepend to routes within the directory.
+   */
+  private handleDirectory(dirPath: string, basePath: string): void {
+    const segment = this.parse_route_segment(path.basename(dirPath));
+    this.build_routes(dirPath, `${basePath}/${segment}`);
+
+    const indexPath = path.join(dirPath, 'index.ts');
+    if (fs.existsSync(indexPath) && this.is_valid_file(indexPath)) {
+      this.processRouteFile(indexPath, dirPath);
+    }
+  }
+
+  /**
+   * Handles individual route files.
+   * @param filePath - The path of the file to handle.
+   * @param baseDir - The base directory path to apply middleware.
+   */
+  private handleFile(filePath: string, baseDir: string): void {
+    this.processRouteFile(filePath, baseDir);
+  }
+
+  /**
+   * Processes a route file to apply middleware and route handlers.
+   * @param filePath - The path of the route file.
+   * @param baseDir - The base directory path to apply middleware.
+   */
+  private processRouteFile(filePath: string, baseDir: string): void {
+    const route = require(filePath).default;
+    const route_path = this.convertToHonoRoute(filePath);
+    this.applyMiddleware(baseDir, route_path);
+
+    Object.keys(route).forEach((method) => {
+      const handler = route[method];
+      if (typeof handler === 'function') {
+        this.applyRouteMethod(method, route_path, handler);
+      }
+    });
+  }
+
+  /**
+   * Applies HTTP route methods to the Hono app.
+   * @param method - The HTTP method (GET, POST, etc.).
+   * @param routePath - The path for the route.
+   * @param handler - The handler function for the route.
+   */
+  private applyRouteMethod(
+    method: string,
+    routePath: string,
+    handler: any
+  ): void {
+    switch (method.toLowerCase()) {
+      case 'get':
+        this.app.get(routePath, handler);
+        break;
+      case 'post':
+        this.app.post(routePath, handler);
+        break;
+      case 'put':
+        this.app.put(routePath, handler);
+        break;
+      case 'delete':
+        this.app.delete(routePath, handler);
+        break;
+      case 'patch':
+        this.app.patch(routePath, handler);
+        break;
+      case '_':
+        this.app.use(routePath, handler);
+        break;
+      default:
+        console.warn(`Unknown method ${method}`);
+    }
+  }
+
+  /**
+   * Starts a file watcher to monitor changes in the middleware and routes directories.
+   * The watcher listens for file additions, modifications, and deletions.
+   * When changes are detected, routes are rebuilt, and global middleware is reapplied without reinitializing the Hono application.
+   */
   private startFileWatcher(): void {
     const watcher = chokidar.watch([
       this.default_middleware_dir,
@@ -172,31 +278,41 @@ class Wiggly {
     ]);
     watcher.on('all', (event, path) => {
       if (['add', 'change', 'unlink'].includes(event)) {
-        this.app = new Hono(this.app);
-        this.applyGlobalMiddleware();
-        this.build_routes();
+        this.applyGlobalMiddleware(); // Reapply global middleware
+        this.build_routes(); // Rebuild routes
       }
     });
   }
 
+  /**
+   * Starts the server and listens for incoming HTTP requests.
+   * Uses the Hono `serve` function to start the server on the specified port.
+   * Initializes the file watcher to monitor changes in middleware and routes directories.
+   * @param port - The port number on which the server will listen. Defaults to 8080.
+   * @param args - Optional arguments to pass to the Hono `serve` function. These can be used to customize server behavior.
+   * @returns A promise that resolves when the server starts successfully.
+   */
   async serve(
     port: number = 8080,
     args?: Parameters<typeof node_serve>
   ): Promise<void> {
     try {
+      this.applyGlobalMiddleware(); // Ensure middleware is applied before starting
+      this.build_routes(); // Ensure routes are built before starting
+
       await node_serve({
         fetch: this.app.fetch,
         port,
         ...args,
       });
-      this.startFileWatcher();
+
+      this.startFileWatcher(); // Start file watcher after server starts
 
       console.log(`Server Running On http://localhost:${port}`);
     } catch (error) {
-      console.error('error');
+      console.error(error);
     }
   }
 }
 
 export default Wiggly;
-//				^?
