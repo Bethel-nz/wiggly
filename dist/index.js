a609d7f5b41b8c547fdbb3c93bbaea9cd7e00d4a
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-07-19 01:07:46 +0100

    fix: package import

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,1 @@
-import Wiggly from './src/lib/wiggly';
-// Initialize Wiggly with base path
-const wiggle = new Wiggly({
-    base_path: '/api/v1/',
-    middleware_dir: 'src/example/routes/middleware',
-    routes_dir: 'src/example/routes',
-});
-wiggle.build_routes();
-wiggle.serve();
+export { default } from './src/lib/wiggly';
